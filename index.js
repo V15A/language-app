cfa6734afe7eba6abbeb038125336b3b702f9bdf
Visa Keskinen <visa.keskinen@gmail.com>
2022-01-06 22:37:00 +0200

    changes to make debugging easier and use async await

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 
 const db = require("./db.js");
-const port = 3000;
+const port = 3050;
 const app = express();
 
-app.get("/test", (req, res) => {
-  let data = db.findAll();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.get("/test", async (req, res) => {
+  let data = await db.findAll();
   res.send(data);
+  console.log(data);
 });
 
 app.listen(port, () => {
