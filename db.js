6e5d845c45eba9ef21740080cf1855e9c8a1f1a6
V15A <visa.k.keskinen@tuni.fi>
2022-01-06 14:26:58 +0200

    create db query file with mysql pool

---
+++ db.js
@@ -0,0 +1,10 @@
+const mysql = require("mysql");
+
+// use pool for connections to db
+const pool = mysql.createPool({
+  connectionlimit: 100,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DB,
+});
