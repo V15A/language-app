8826df0f772b436480bfda67aa7c1eb8cfd00422
V15A <visa.k.keskinen@tuni.fi>
2022-01-06 14:31:28 +0200

    add test getter

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require("express");
 const port = 3000;
 const app = express();
 
+app.get("/test", (req, res) => {
+  res.send("Test yeh.");
+});
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
