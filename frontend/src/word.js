5397a1427f7151b8a013ad48565eb520c360b86b
Visa Keskinen <visa.keskinen@gmail.com>
2022-01-07 19:30:54 +0200

    implement word checking on submit button click

--- frontend/src/word.js
+++ frontend/src/word.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function WordElement(props) {
+  const [correct, setCorrect] = useState(0);
+  const [quess, setQuess] = useState("");
+
+  const handleSubmit = () => {
+    if (quess === props.finnish) {
+      setCorrect(1);
+      console.log(correct);
+    } else {
+      setCorrect(-1);
+      console.log(correct);
+    }
+  };
+
+  const renderFeedback = () => {
+    if (correct === 1) {
+      return <span>Correct</span>;
+    } else if (correct === -1) {
+      return <span>Wrong</span>;
+    }
+  };
+
+  const handleChange = (event) => {
+    setQuess(event.target.value);
+  };
+
   return (
-    <div>
+    <div key={props.id}>
       {" "}
       <span>{props.english} </span>
-      <input placeholder="in finnish"></input>{" "}
-      <button onClick={() => {}}>Submit</button>
+      <input
+        placeholder="in finnish"
+        value={quess}
+        onChange={handleChange}
+      ></input>{" "}
+      <button onClick={handleSubmit}>Submit</button> {renderFeedback()}
     </div>
   );
 }
