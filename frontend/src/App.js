964bb649d6a1ee29251d7ec570cadb5228ade447
Visa Keskinen <visa.keskinen@gmail.com>
2022-01-07 16:54:08 +0200

    modify data fetching and render content when fetch is ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,43 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  async function FetchData() {
-    let res = await fetch("http://localhost:3050/test");
-    const json = await res.json();
-    console.log(json);
-    return json;
+  useEffect(() => {
+    async function FetchData() {
+      try {
+        let res = await fetch("http://localhost:3050/test");
+        const json = await res.json();
+        console.log(json);
+        setData(json);
+        setIsLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    FetchData();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div className="App">
+        <h1>FRONTEND FRONT PAGE</h1>
+        <form onSubmit={() => {}}>
+          <span>{data[1].english} </span>
+          <input placeholder="in finnish"></input>{" "}
+          <button onClick={() => {}}>Submit</button>
+          <br />
+          <span>{data[0].english} </span>
+          <input placeholder="in finnish"></input>{" "}
+          <button onClick={() => {}}>Submit</button>
+        </form>
+      </div>
+    );
   }
-
-  setData(FetchData());
-  return (
-    <div className="App">
-      <p>{data[1].english}</p>
-    </div>
-  );
 }
 
 export default App;
