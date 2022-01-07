29b31566237deacf976932111e90d5fc3f59c40c
Visa Keskinen <visa.keskinen@gmail.com>
2022-01-07 23:10:05 +0200

    add post for adding new data to database

--- index.js
+++ index.js
@@ -3,16 +3,32 @@ const express = require("express");
 const db = require("./db.js");
 const port = 3050;
 const app = express();
+app.use(express.json());
 
+// for debugging so that react app can access the db
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+  });
   next();
 });
 
-app.get("/test", async (req, res) => {
+app.get("/", async (req, res) => {
   let data = await db.findAll();
   res.send(data);
   console.log(data);
+});
+
+app.post("/add", async (req, res) => {
+  try {
+    let data = await db.save(req.body);
+    console.log("adding");
+    console.log(req.body);
+    res.send(data);
+  } catch (err) {
+    console.log(err.message);
+  }
 });
 
 app.listen(port, () => {
