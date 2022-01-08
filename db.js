d267bb0096b7b751f254d5f126878203effc7d11
Visa Keskinen <visa.keskinen@gmail.com>
2022-01-08 14:54:43 +0200

    create deleteById query function

--- db.js
+++ db.js
@@ -39,6 +39,17 @@ let connectionFunctions = {
       });
     });
   },
+  deleteById: (id) => {
+    return new Promise((resolve, reject) => {
+      pool.query("DELETE * FROM words WHERE id = ?", [id], (err, res) => {
+        if (err) {
+          console.log(err.message);
+          return reject(err);
+        }
+        resolve(res);
+      });
+    });
+  },
   findByTag: (tag) => {},
 };
 
