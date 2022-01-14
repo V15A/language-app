d957c3a3fba6ae9aff5cccd4989125ddfca043eb
Visa Keskinen <visa.keskinen@gmail.com>
2022-01-15 00:14:42 +0200

    Fix parent component updating when deleting

--- frontend/src/deleteWord.js
+++ frontend/src/deleteWord.js
@@ -1,9 +1,10 @@
-async function DeleteWord(id) {
+async function DeleteWord(props) {
   try {
-    let res = await fetch(`http://localhost:3050/words/delete/${id}`, {
+    let res = await fetch(`http://localhost:3050/words/delete/${props.id}`, {
       method: "DELETE",
     });
     console.log(res.status);
+    props.update();
   } catch (err) {
     console.log(err.message);
   }
